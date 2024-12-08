0916dc6dcb65e3b094f33fa6d01a517bbc7c8fef
frigom <matteo.frigo@tngtech.com>
2024-12-08 18:49:50 +0100

    update default

--- default/day0.go
+++ default/day0.go
@@ -21,9 +21,42 @@ func readLines(filename string) []string {
 	return lines
 }
 
+func readGrid(filename string) [][]string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	lines := []string{}
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	grid := [][]string{}
+	for _, line := range lines {
+		gridLine := []string{}
+		for _, char := range line {
+			gridLine = append(gridLine, string(char))
+		}
+		grid = append(grid, gridLine)
+	}
+	return grid
+}
+
+func printGrid(grid [][]string) {
+	for _, line := range grid {
+		for _, char := range line {
+			fmt.Print(string(char))
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
-	//inputFile := "inputtest2"
-	inputFile := "input"
+	inputFile := "inputtest"
+	//inputFile := "input"
 	lines := readLines(inputFile)
 	fmt.Println(lines)
+	grid := readGrid(inputFile)
+	printGrid(grid)
 }
